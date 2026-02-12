a16567427e31e1ea0d2c87d8b7aa2b88bf9f24c7
FEX156 <taufiqh7h032@gmail.com>
2026-02-12 23:46:57 +0700

    setup is project

---
+++ logic_nolep/project/ln-inventory/src/applications/web.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import "dotenv/config";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+export default app;
